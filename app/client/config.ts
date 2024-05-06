ca0a013c9e81bb66b120b93bb1f7f2f48f5923a5
huanken <sinhhuan2109@gmail.com>
2024-05-06 22:35:45 +0700

    [todo_app] config ts client

---
+++ app/client/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'wd4tkw4j0c'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+    domain: 'dev-hm2pq1mf0vak3xhr.us.auth0.com',    // Domain from Auth0
+    clientId: 'y7gRNvCy0nmdnFIc5ay76zSajrL0i3JW',  // Client id from an Auth0 application
+    callbackUrl: 'http://localhost:3000/'
+  }
